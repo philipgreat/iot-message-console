833ee49b7d25510e0341a859f0163f8faac9352f
Philip Zhang <zhangxilai@doublechaintech.com>
2022-11-12 01:50:34 +0800

    🚀fine

---
+++ .history/iotbump_20171110134058.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func decode(b []byte, length int) ([]byte, error) {
+
+	buf := make([]byte, length)
+	for i := 0; i < length; i++ {
+		buf[i] = 255 - b[i]
+	}
+	return buf, nil
+
+}
+func checkError(err error) {
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(0)
+	}
+}
+func castIoTMessage(hub *Hub) {
+	ServerAddr, err := net.ResolveUDPAddr("udp", ":54321")
+	checkError(err)
+
+	/* Now listen at selected port */
+	ServerConn, err := net.ListenUDP("udp", ServerAddr)
+	checkError(err)
+	defer ServerConn.Close()
+
+	buf := make([]byte, 1024)
+
+	for {
+		n, addr, err := ServerConn.ReadFromUDP(buf)
+		if err != nil {
+			fmt.Println("Error: ", err)
+			continue
+		}
+		decodedBuffer, err := decode(buf, n)
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+		//fmt.Println("Received ", string(decodedBuffer[0:n]), " from ", addr)
+		fmt.Println(addr, " ", string(decodedBuffer[0:n]))
+		hub.broadcast <- decodedBuffer[0:n]
+
+	}
+}
